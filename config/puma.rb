831ee9979ae6c6aa86e1d7884828f91d328d32bd
kimsinhbunbu <sinh.kim.phan@bunbu.vn>
2026-04-15 16:07:05 +0700

    final big fix

--- config/puma.rb
+++ config/puma.rb
@@ -26,6 +26,9 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+# Bind to a Unix socket for Nginx
+bind "unix:///home/bunbu/landing_page/current/tmp/sockets/puma.sock"
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
