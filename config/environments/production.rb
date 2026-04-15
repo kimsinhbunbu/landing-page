2f18499884bbe6aa45d8db2a5deb64e351466927
kimsinhbunbu <sinh.kim.phan@bunbu.vn>
2026-04-15 16:43:13 +0700

    final big fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
+  # Fix InvalidAuthenticityToken due to HTTPS/HTTP proxy mismatch
+  config.action_controller.forgery_protection_origin_check = false
+
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
