8185ec31d85177ce0398c9a7b6b569f87a4378c9
kimsinhbunbu <sinh.kim.phan@bunbu.vn>
2026-04-15 16:30:39 +0700

    final big fix

--- config/boot.rb
+++ config/boot.rb
@@ -18,3 +18,17 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Hack for CentOS 7 (SQLite 3.7.17 < 3.8)
+begin
+  require 'active_record/connection_adapters/sqlite3_adapter'
+  module ActiveRecord
+    module ConnectionAdapters
+      class SQLite3Adapter
+        private
+        def check_version(connection); end
+      end
+    end
+  end
+rescue LoadError
+end
